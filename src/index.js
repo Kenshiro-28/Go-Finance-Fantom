c67fd0b8851c85011a2efb8fdac06477cfbebd3a
Kenshiro <>
2022-04-28 16:33:07 +0200

    Update to React v18

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';  
 import ReactDOM from 'react-dom';  
-import {HashRouter as Router, Route, NavLink, Switch} from 'react-router-dom'   
+import {HashRouter as Router, Route, NavLink, Routes} from 'react-router-dom'   
 import 'bootstrap/dist/css/bootstrap.css'
 
 import Home from './components/Home';
@@ -27,15 +27,15 @@ const routing = (
 		   <div className="col-sm">
 			   <NavLink to="/farm" exact>合 Gō Farm</NavLink>  
 		   </div>
-
+		   
    		   <div className="col-sm">
 			   <NavLink to="/ginko" exact>銀行 Ginkō</NavLink>  
 		   </div>
-		   		   
+		   
  		   <div className="col-sm-2">
 			   <NavLink to="/shogun" exact>将軍 Shōgun Pool</NavLink>  
 		   </div>
-	   
+		   
    		   <div className="col-sm">
 			   <NavLink to="/rei" exact>霊 Rei Pool</NavLink>  
 		   </div>
@@ -43,30 +43,31 @@ const routing = (
    		   <div className="col-sm">
 			   <NavLink to="/ki" exact>気 Ki Pool</NavLink>  
 		   </div>
-			   
+		   
 		   <div className="col-sm">
 			   <NavLink to="/vault" exact>金庫 Vault</NavLink>  
 		   </div>
-	   
+		   
 		   <div className="col-sm">
 			   <NavLink to="/about" exact>情報 About</NavLink>  
 		   </div>
 		   
 	    </div>
         <div>  
-            <Switch>  
-                <Route exact path="/" component={Home} />  
-                <Route exact path="/farm" component={Farm} />
-                <Route exact path="/ginko" component={Ginko} />                  
-                <Route exact path="/shogun" component={ShogunPool} />
-                <Route exact path="/rei" component={ReiPool} />
-                <Route exact path="/ki" component={KiPool} />
-                <Route exact path="/vault" component={Vault} />
-                <Route exact path="/about" component={About} />  
-                <Route component={Notfound} />  
-            </Switch>  
+            <Routes>  
+                <Route path="/" element={<Home/>} />
+                <Route path="/farm" element={<Farm/>} />
+                <Route path="/ginko" element={<Ginko/>} />                
+                <Route path="/shogun" element={<ShogunPool/>} />
+                <Route path="/rei" element={<ReiPool/>} />
+                <Route path="/ki" element={<KiPool/>} />
+                <Route path="/vault" element={<Vault/>} />    
+                <Route path="/about" element={<About/>} />  
+                <Route path="*" element={<Notfound/>} />  
+            </Routes>  
         </div>  
     </Router> 
 )  
 
 ReactDOM.render(routing, document.getElementById('root'));
+
