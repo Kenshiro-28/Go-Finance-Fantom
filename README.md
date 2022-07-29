a0df0afb5a2ce4f02ee00ceaa79b4f5441197b85
Kenshiro <>
2022-07-29 19:37:02 +0200

    Update UI

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Gō Finance
 
-Gō Finance is a 100% decentralized DeFi platform with an open source web interface that anyone can download and run locally on their computer.
+Gō Finance is a cooperative DeFi platform. To use the farms, users must contribute to the shared fund by paying deposit fees. Smart contracts use the shared fund to automatically execute custom strategies that generate rewards for all depositors.
 
-Smart contracts have no owner privileges and run an immutable set of rules. They are deployed on BSC, Polygon and Fantom.
+The web interface is open source and anyone can download and run it locally on their computer. Smart contracts have no owner privileges and run an immutable set of rules. They are deployed on BSC, Polygon and Fantom.
 
 Keep in mind that trading is a risky activity that can involve a loss of money. You should only invest the amount you can afford to lose.
 
 ## Tokenomics
 
-fGō is a fully decentralized token with limited supply and very slow issuance, which makes it a good store of value. The name comes from the gō (合) or cup, which is a traditional Japanese unit used for a serving of rice and a cup of sake in Japanese cuisine.
+fGō is a fully decentralized token that can be staked to earn rewards. There are performance fees on some strategies that buy fGō from the market, which helps increase its price. The name comes from the gō (合) or cup, which is a traditional Japanese unit used for a serving of rice and a cup of sake in Japanese cuisine.
 
 Token name: fGō
 
@@ -18,17 +18,17 @@ Contract: 0x827a19692B8BcEa675a8Bb5791048b2E2E616F16
 
 ## Gō farm
 
-You can stake your fGō tokens to earn fGō rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.
+You can deposit your fGō tokens to earn fGō rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.
 
-You can harvest up to one time per day, but the maximum staking power is reached after one year without harvesting. Keep in mind that your rewards may suddenly increase or decrease depending on how much other users harvest and the total deposits on the farm, so the most conservative approach is frequent harvests.
+You can harvest up to one time per day, but the maximum staking power is reached after one year without harvesting, at which point you must harvest to continue earning rewards.
 
 Contract: 0xc99a8C938efFE3725952Ba083F624C364ed717FC
 
 ## Ginkō
 
-You can stake your DAI tokens to earn DAI rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.
+You can deposit your DAI tokens to earn DAI rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.
 
-You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting. Keep in mind that your rewards may suddenly increase or decrease depending on how much other users harvest and the total deposits on the contract, so the most conservative approach is frequent harvests.
+You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting, at which point you must harvest to continue earning rewards.
 
 Deposit fees are used to buy TSHARE and deposit it in the Tomb Finance Masonry. Two-thirds of the TOMB rewards earned on Tomb Finance are used to buy DAI and deposit it in the rewards fund. The remaining third of the rewards are reinvested in the Masonry, increasing performance over time.
 
@@ -36,9 +36,9 @@ Contract: 0x34b67c2D8fC0c01Af91397b80d2E099138275FEB
 
 ## Shōgun Pool
 
-You can stake your fGō tokens to earn DAI rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.
+You can deposit your fGō tokens to earn DAI rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.
 
-You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting. Keep in mind that your rewards may suddenly increase or decrease depending on how much other users harvest and the total deposits on the contract, so the most conservative approach is frequent harvests.
+You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting, at which point you must harvest to continue earning rewards.
 
 Deposit fees are used to buy BSHARE and deposit it in the Based Finance Acropolis. Two-thirds of the BASED rewards earned on Based Finance are used to buy DAI and deposit it in the rewards fund. The remaining third of the rewards are reinvested in the Acropolis, increasing performance over time.
 
@@ -46,9 +46,9 @@ Contract: 0x8DEEBDa6AA4d8c86653bB984e647ef5dEE575982
 
 ## Rei Pool
 
-You can stake your fGō tokens to earn AAVE rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.
+You can deposit your fGō tokens to earn AAVE rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.
 
-You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting. Keep in mind that your rewards may suddenly increase or decrease depending on how much other users harvest and the total deposits on the contract, so the most conservative approach is frequent harvests.
+You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting, at which point you must harvest to continue earning rewards.
 
 Deposit fees are used to buy BSHARE and deposit it in the Based Finance Acropolis. Two-thirds of the BASED rewards earned on Based Finance are used to buy AAVE and deposit it in the rewards fund. The remaining third of the rewards are reinvested in the Acropolis, increasing performance over time.
 
@@ -56,9 +56,9 @@ Contract: 0x1eAcE63DdAAFAd808f0FE4757774f0d7f3328A09
 
 ## Ki Pool
 
-You can stake your fGō tokens to earn 2OMB rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.
+You can deposit your fGō tokens to earn 2OMB rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.
 
-You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting. Keep in mind that your rewards may suddenly increase or decrease depending on how much other users harvest and the total deposits on the contract, so the most conservative approach is frequent harvests.
+You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting, at which point you must harvest to continue earning rewards.
 
 Deposit fees are used to buy BSHARE and deposit it in the Based Finance Acropolis. Two-thirds of the BASED rewards earned on Based Finance are used to buy 2OMB and deposit it in the rewards fund. The remaining third of the rewards are reinvested in the Acropolis, increasing performance over time.
 
@@ -66,9 +66,9 @@ Contract: 0x939f6DC232Bc5e02b336Fd1C6B46E2A516D442B0
 
 ## Vault
 
-You can stake your Olympus governance tokens (gOHM) to earn gOHM rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.
+You can deposit your Olympus governance tokens (gOHM) to earn gOHM rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.
 
-You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting. Keep in mind that your rewards may suddenly increase or decrease depending on how much other users harvest and the total deposits on the contract, so the most conservative approach is frequent harvests.
+You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting, at which point you must harvest to continue earning rewards.
 
 Deposit fees are used to buy TSHARE and deposit it in the Tomb Finance Masonry. Two-thirds of the TOMB rewards earned on Tomb Finance are used to buy gOHM and deposit it in the rewards fund. The remaining third of the rewards are reinvested in the Masonry, increasing performance over time.
 
--- src/components/About.js
+++ src/components/About.js
@@ -13,24 +13,24 @@ class About extends Component
               <div className="col-sm">
                 <p className="centered_text">----- Overview -----</p>
                 <br />
-          		<p>Gō Finance is a 100% decentralized DeFi platform with an open source web interface that anyone can download and run locally on their computer.</p>
-          		<p>Smart contracts have no owner privileges and run an immutable set of rules. They are deployed on BSC, Polygon and Fantom.</p>
+          		<p>Gō Finance is a cooperative DeFi platform. To use the farms, users must contribute to the shared fund by paying deposit fees. Smart contracts use the shared fund to automatically execute custom strategies that generate rewards for all depositors.</p>
+          		<p>The web interface is open source and anyone can download and run it locally on their computer. Smart contracts have no owner privileges and run an immutable set of rules. They are deployed on BSC, Polygon and Fantom.</p>
           		<p>Keep in mind that trading is a risky activity that can involve a loss of money. You should only invest the amount you can afford to lose.</p>
           		<br /><p />
           		<p className="centered_text">----- Tokenomics -----</p>
           		<br />
-          		<p>fGō is a fully decentralized token with limited supply and very slow issuance, which makes it a good store of value. The name comes from the gō (合) or cup, which is a traditional Japanese unit used for a serving of rice and a cup of sake in Japanese cuisine.</p>
+          		<p>fGō is a fully decentralized token that can be staked to earn rewards. There are performance fees on some strategies that buy fGō from the market, which helps increase its price. The name comes from the gō (合) or cup, which is a traditional Japanese unit used for a serving of rice and a cup of sake in Japanese cuisine.</p>
           		<br />
           		<p>Token name: &nbsp;&nbsp;fGō</p>
           		<p>Total supply: &nbsp;&nbsp;1 million</p>
-          		<p>Contract: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          		<p>Contract: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           		      0x827a19692B8BcEa675a8Bb5791048b2E2E616F16</p>
           		<br /><p />
           		<p className="centered_text">----- 合 Gō Farm -----</p>
           		<br />
-          		<p>You can stake your fGō tokens to earn fGō rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.</p>
+          		<p>You can deposit your fGō tokens to earn fGō rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.</p>
           		
-          		<p>You can harvest up to one time per day, but the maximum staking power is reached after one year without harvesting. Keep in mind that your rewards may suddenly increase or decrease depending on how much other users harvest and the total deposits on the farm, so the most conservative approach is frequent harvests.</p>
+          		<p>You can harvest up to one time per day, but the maximum staking power is reached after one year without harvesting, at which point you must harvest to continue earning rewards.</p>
           		
           		<br />
           		<p>Contract: &nbsp;&nbsp;0xc99a8C938efFE3725952Ba083F624C364ed717FC</p>
@@ -38,9 +38,9 @@ class About extends Component
           		
           		<p className="centered_text">----- 銀行 Ginkō -----</p>
           		<br />
-          		<p>You can stake your DAI tokens to earn DAI rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.</p>
+          		<p>You can deposit your DAI tokens to earn DAI rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.</p>
           		
-          		<p>You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting. Keep in mind that your rewards may suddenly increase or decrease depending on how much other users harvest and the total deposits on the contract, so the most conservative approach is frequent harvests.</p>
+          		<p>You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting, at which point you must harvest to continue earning rewards.</p>
           		
           		<p>Deposit fees are used to buy TSHARE and deposit it in the Tomb Finance Masonry. Two-thirds of the TOMB rewards earned on Tomb Finance are used to buy DAI and deposit it in the rewards fund. The remaining third of the rewards are reinvested in the Masonry, increasing performance over time.</p>
           		
@@ -50,9 +50,9 @@ class About extends Component
           		
           		<p className="centered_text">----- 将軍 Shōgun Pool -----</p>
           		<br />
-          		<p>You can stake your fGō tokens to earn DAI rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.</p>
+          		<p>You can deposit your fGō tokens to earn DAI rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.</p>
           		
-          		<p>You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting. Keep in mind that your rewards may suddenly increase or decrease depending on how much other users harvest and the total deposits on the contract, so the most conservative approach is frequent harvests.</p>
+          		<p>You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting, at which point you must harvest to continue earning rewards.</p>
           		
           		<p>Deposit fees are used to buy BSHARE and deposit it in the Based Finance Acropolis. Two-thirds of the BASED rewards earned on Based Finance are used to buy DAI and deposit it in the rewards fund. The remaining third of the rewards are reinvested in the Acropolis, increasing performance over time.</p>
           		
@@ -62,9 +62,9 @@ class About extends Component
 
           		<p className="centered_text">----- 霊 Rei Pool -----</p>
           		<br />
-          		<p>You can stake your fGō tokens to earn AAVE rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.</p>
+          		<p>You can deposit your fGō tokens to earn AAVE rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.</p>
           		
-          		<p>You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting. Keep in mind that your rewards may suddenly increase or decrease depending on how much other users harvest and the total deposits on the contract, so the most conservative approach is frequent harvests.</p>
+          		<p>You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting, at which point you must harvest to continue earning rewards.</p>
           		
           		<p>Deposit fees are used to buy BSHARE and deposit it in the Based Finance Acropolis. Two-thirds of the BASED rewards earned on Based Finance are used to buy AAVE and deposit it in the rewards fund. The remaining third of the rewards are reinvested in the Acropolis, increasing performance over time.</p>
           		
@@ -74,9 +74,9 @@ class About extends Component
 
           		<p className="centered_text">----- 気 Ki Pool -----</p>
           		<br />
-          		<p>You can stake your fGō tokens to earn 2OMB rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.</p>
+          		<p>You can deposit your fGō tokens to earn 2OMB rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.</p>
           		
-          		<p>You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting. Keep in mind that your rewards may suddenly increase or decrease depending on how much other users harvest and the total deposits on the contract, so the most conservative approach is frequent harvests.</p>
+          		<p>You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting, at which point you must harvest to continue earning rewards.</p>
           		
           		<p>Deposit fees are used to buy BSHARE and deposit it in the Based Finance Acropolis. Two-thirds of the BASED rewards earned on Based Finance are used to buy 2OMB and deposit it in the rewards fund. The remaining third of the rewards are reinvested in the Acropolis, increasing performance over time.</p>
           		
@@ -86,9 +86,9 @@ class About extends Component
           		
           		<p className="centered_text">----- 金庫 Vault -----</p>
           		<br />
-          		<p>You can stake your Olympus governance tokens (gOHM) to earn gOHM rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.</p>
+          		<p>You can deposit your Olympus governance tokens (gOHM) to earn gOHM rewards that are variable and subject to the dynamics of supply and demand. Your rewards increase with your staking power, which depends on how often you harvest your rewards: the longer the time between harvests, the higher the staking power.</p>
           		
-          		<p>You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting. Keep in mind that your rewards may suddenly increase or decrease depending on how much other users harvest and the total deposits on the contract, so the most conservative approach is frequent harvests.</p>
+          		<p>You can harvest up to one time per day, but the maximum staking power is reached after one month without harvesting, at which point you must harvest to continue earning rewards.</p>
           		
           		<p>Deposit fees are used to buy TSHARE and deposit it in the Tomb Finance Masonry. Two-thirds of the TOMB rewards earned on Tomb Finance are used to buy gOHM and deposit it in the rewards fund. The remaining third of the rewards are reinvested in the Masonry, increasing performance over time.</p>
           		
